d6f309cd19c17b3df5ed5c5ad0a4f270f2e73a07
Himanshu Singhal <developer.singhal@gmail.com>
2024-06-25 20:04:53 +0530

    Added villa api controller and get method

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        // Http verbs helps swagger to create the proper documentation. If there is no httpverb swagger page will give an error
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas() 
+        {
+            return new List<Villa> {
+                new Villa { Id = 1, Name = "Pool View" },
+                new Villa { Id = 2, Name = "Beach View" }
+            };
+        }
+    }
+}
