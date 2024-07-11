aa4acf61d55445f91a0d803a915c4698dd982c66
Himanshu Singhal <developer.singhal@gmail.com>
2024-07-11 18:11:33 +0530

    Commented all the custom code

--- MagicVilla_VillaAPI/OpenApi/ConfigureSwaggerGenOptions.cs
+++ MagicVilla_VillaAPI/OpenApi/ConfigureSwaggerGenOptions.cs
@@ -1,35 +1,35 @@
-﻿using Asp.Versioning.ApiExplorer;
-using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
+﻿//using Asp.Versioning.ApiExplorer;
+//using Microsoft.Extensions.Options;
+//using Microsoft.OpenApi.Models;
+//using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace MagicVilla_VillaAPI.OpenApi
-{
-    // Created this file by watching the youtube video for versioning instructions
-    public class ConfigureSwaggerGenOptions : IConfigureNamedOptions<SwaggerGenOptions>
-    {
-        private readonly IApiVersionDescriptionProvider _provider;
-        public ConfigureSwaggerGenOptions(IApiVersionDescriptionProvider provider)
-        {
-            _provider = provider;
-        }
-        public void Configure(string name, SwaggerGenOptions options)
-        {
-            Configure(options);
-        }
+//namespace MagicVilla_VillaAPI.OpenApi
+//{
+//    // Created this file by watching the youtube video for versioning instructions
+//    public class ConfigureSwaggerGenOptions : IConfigureNamedOptions<SwaggerGenOptions>
+//    {
+//        private readonly IApiVersionDescriptionProvider _provider;
+//        public ConfigureSwaggerGenOptions(IApiVersionDescriptionProvider provider)
+//        {
+//            _provider = provider;
+//        }
+//        public void Configure(string name, SwaggerGenOptions options)
+//        {
+//            Configure(options);
+//        }
 
-        public void Configure(SwaggerGenOptions options)
-        {
-            foreach (ApiVersionDescription description in _provider.ApiVersionDescriptions)
-            {
-                var openApiInfo = new OpenApiInfo()
-                {
-                    Title = $"Magic Villa API v{description.ApiVersion}",
-                    Version = description.ApiVersion.ToString()
-                };
+//        public void Configure(SwaggerGenOptions options)
+//        {
+//            foreach (ApiVersionDescription description in _provider.ApiVersionDescriptions)
+//            {
+//                var openApiInfo = new OpenApiInfo()
+//                {
+//                    Title = $"Magic Villa API v{description.ApiVersion}",
+//                    Version = description.ApiVersion.ToString()
+//                };
 
-                options.SwaggerDoc(description.GroupName, openApiInfo);
-            }   
-        }
-    }
-}
+//                options.SwaggerDoc(description.GroupName, openApiInfo);
+//            }   
+//        }
+//    }
+//}
