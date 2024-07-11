94f2afc75b23f3a391a6894271b1dea717e119d5
Himanshu Singhal <developer.singhal@gmail.com>
2024-07-11 18:10:39 +0530

    Added nuget packages for versioning and added custom code

---
+++ MagicVilla_VillaAPI/OpenApi/ConfigureSwaggerGenOptions.cs
@@ -0,0 +1,35 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MagicVilla_VillaAPI.OpenApi
+{
+    // Created this file by watching the youtube video for versioning instructions
+    public class ConfigureSwaggerGenOptions : IConfigureNamedOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+        public ConfigureSwaggerGenOptions(IApiVersionDescriptionProvider provider)
+        {
+            _provider = provider;
+        }
+        public void Configure(string name, SwaggerGenOptions options)
+        {
+            Configure(options);
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (ApiVersionDescription description in _provider.ApiVersionDescriptions)
+            {
+                var openApiInfo = new OpenApiInfo()
+                {
+                    Title = $"Magic Villa API v{description.ApiVersion}",
+                    Version = description.ApiVersion.ToString()
+                };
+
+                options.SwaggerDoc(description.GroupName, openApiInfo);
+            }   
+        }
+    }
+}
